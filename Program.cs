82f6b3d287f640899e082046b5b69b1da3ef81fe
Haritha Denuwan <harithadenuwan01@gmail.com>
2023-06-04 21:28:04 +1000

    Final update before demonstration

--- Program.cs
+++ Program.cs
@@ -12,14 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// -- ADO Persistence Layer --
-// builder.Services.AddScoped<IRobotCommandDataAccess, RobotCommandADO>();
-// builder.Services.AddScoped<IMapDataAccess, MapADO>();
-
-// -- FastMember Persistence Layer --
-// builder.Services.AddScoped<IRobotCommandDataAccess, RobotCommandRepository>();
-// builder.Services.AddScoped<IMapDataAccess, MapRepository>();
-
 // -- EF Persistence Layer --
 builder.Services.AddDbContext<GalleryContext>();
 builder.Services.AddScoped<IArtifactDataAccess, ArtifactEF>();
